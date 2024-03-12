d7da5478853dbb5ba912e46459acc93c6105c28f
Hubert Materek <s25160@pjwstk.edu.pl>
2024-03-12 11:30:34 +0100

    Print avg

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -4,3 +4,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double GetAvg(int[] numb)
+{
+    int sum = 0;
+    foreach (var num in numb)
+    {
+        sum += num;
+    }
+
+    return (double)sum / numb.Length;
+}
+
+int[] numbers = {1,2,3,4};
+double avg = GetAvg(numbers);
+Console.WriteLine(avg);
